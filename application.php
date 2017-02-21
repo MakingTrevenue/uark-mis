fce32f80c24fe9a6d24732a6127f6f487d49a92d
nwsm <eknewsom@uark.edu>
2017-02-21 13:07:03 -0600

    Added file uploading to applicaiton.php.

--- application.php
+++ application.php
@@ -73,4 +73,22 @@
 		}
 		echo "</table>";	
 	}
+
+	$target_dir = "../private/uploads/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["resumeFile"])) {
+		$uploadOk=1;
+		/*
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+			echo "File is an image - " . $check["mime"] . ".";
+			$uploadOk = 1;
+		} else {
+			echo "File is not an image.";
+			$uploadOk = 0;
+		}*/
+	}
 ?>
