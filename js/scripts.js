18fa1a3731fe10506a0364e056085b8ab54dfbb5
Trevor Darby <tdarby51@gmail.com>
2017-02-21 13:27:49 -0600

    js scripts update

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+//Duplicate Function
+document.getElementById('duplicateButton').onclick = duplicate;
+
+
+var i = 0;
+var original = document.getElementById('collegeDuplicate');
+
+function duplicate() {
+    var clone = original.cloneNode(true); // "deep" clone
+    clone.id = "collegeDuplicate" + ++i; // there can only be one element with an ID
+    original.parentNode.appendChild(clone);
+}
