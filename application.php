a7a3a6fa4d174fdc18dfa96eb4d6e406fcb3bd47
nwsm <eknewsom@uark.edu>
2017-02-21 13:27:08 -0600

    Jk now works?

--- application.php
+++ application.php
@@ -82,7 +82,7 @@
 	if(isset($_POST["submit"])) {
 		$uploadOk=1;
 		
-		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		$check = getimagesize($_FILES["resumeFile"]["tmp_name"]);
 		if($check !== false) {
 			echo "File is an image - " . $check["mime"] . ".";
 			$uploadOk = 1;
@@ -91,8 +91,8 @@
 			$uploadOk = 1;
 		}
 	}
-	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	if (move_uploaded_file($_FILES["resumeFile"]["tmp_name"], $target_file)) {
+		echo "The file ". basename( $_FILES["resumeFile"]["name"]). " has been uploaded.";
 	} else {
 		echo "Sorry, there was an error uploading your file.";
 	}	
