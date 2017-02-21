a84f12bf0e6bd8db0e7854d98542e2c12bd2ca33
nwsm <eknewsom@uark.edu>
2017-02-21 13:25:17 -0600

    Actually upload file :)

--- application.php
+++ application.php
@@ -91,6 +91,10 @@
 			$uploadOk = 1;
 		}
 	}
-	echo "file supposedly submitted lel";
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	} else {
+		echo "Sorry, there was an error uploading your file.";
+	}	
 
 ?>
