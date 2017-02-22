2502eed9e56118f335f086a57c7f775527fc3e98
nwsm <eknewsom@uark.edu>
2017-02-21 20:49:39 -0600

    Added signup.php

--- signup.php
+++ signup.php
@@ -73,30 +73,4 @@
 		}
 		echo "</table>";	
 	}
-	/*	UPLOAD FILE
-		$target_dir = "../private/uploads/";
-		//$target_dir="/";
-		$target_file = $target_dir . basename($_FILES["resumeFile"]["name"]);
-		$uploadOk = 1;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-		// Check if image file is a actual image or fake image
-		if(isset($_POST["submit"])) {
-			$uploadOk=1;
-			
-			$check = getimagesize($_FILES["resumeFile"]["tmp_name"]);
-			if($check !== false) {
-				echo "File is an image - " . $check["mime"] . ".";
-				$uploadOk = 1;
-			} else {
-				echo "File is not an image.";
-				$uploadOk = 1;
-			}
-		}
-		if (move_uploaded_file($_FILES["resumeFile"]["tmp_name"], $target_file)) {
-			echo "The file ". basename( $_FILES["resumeFile"]["name"]). " has been uploaded.";
-		} else {
-			echo "Sorry, there was an error uploading your file.";
-		}
-	*/	
-
 ?>
