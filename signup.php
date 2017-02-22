fe052a3006f38ebf45345d3a41a72dbfdb8b7544
nwsm <eknewsom@uark.edu>
2017-02-21 21:18:31 -0600

    Insert signup post data to database

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 	
-	if(false){
+	if(isset($_POST)){
 		$config = parse_ini_file('../private/credentials.ini');
 		$servername = $config["servername"];
 		$username = $config["username"];
@@ -11,66 +11,26 @@
 		try {
 			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$stmt = $conn->prepare("INSERT INTO students (studentID,   firstName,  middleName,  lastName,  preferredName,  primaryEmail,  secondaryEmail,  primaryPhone,  secondaryPhone,  socialSecurityNumber,  dateOfBirth,  ethnicity,  gender,  citizenship) 
-												  VALUES (:studentID, :firstName, :middleName, :lastName, :preferredName, :primaryEmail, :secondaryEmail, :primaryPhone, :secondaryPhone, :socialSecurityNumber, :dateOfBirth, :ethnicity, :gender, :citizenship)");
-			$stmt->bindParam(':studentID', $studentID);
-			$stmt->bindParam(':firstName', $firstName);
-			$stmt->bindParam(':middleName', $middleName);
-			$stmt->bindParam(':lastName', $lastName);
-			$stmt->bindParam(':preferredName', $preferredName);
-			$stmt->bindParam(':primaryEmail', $primaryEmail);
-			$stmt->bindParam(':secondaryEmail', $secondaryEmail);
-			$stmt->bindParam(':primaryPhone', $primaryPhone);
-			$stmt->bindParam(':secondaryPhone', $secondaryPhone);
-			$stmt->bindParam(':socialSecurityNumber', $socialSecurityNumber);
-			$stmt->bindParam(':dateOfBirth', $dateOfBirth);
-			$stmt->bindParam(':ethnicity', $ethnicity);	
-			$stmt->bindParam(':gender', $gender);
-			$stmt->bindParam(':citizenship', $citizenship);				
+			$stmt = $conn->prepare("INSERT INTO users (name,  email,  username,  password) 
+											  VALUES (:name, :email, :username, :password)");
+			$stmt->bindParam(':name', $name);
+			$stmt->bindParam(':email', $email);
+			$stmt->bindParam(':username', $un);
+			$stmt->bindParam(':password', $userpassword);	
+            
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$un = $_POST['username'];
+			$userpassword = $_POST['password'];
+            $userpassword = password_hash($userpassword,PASSWORD_DEFAULT);
 
-			$studentID = $_POST['studentID'];
-			$firstName = $_POST['firstName'];
-			$middleName = $_POST['middleName'];
-			$lastName = $_POST['lastName'];
-			$preferredName = $_POST['preferredName'];
-			$primaryEmail = $_POST['primaryEmail'];
-			$secondaryEmail = $_POST['secondaryEmail'];
-			$primaryPhone = $_POST['primaryPhone'];
-			$secondaryPhone = $_POST['secondaryPhone'];
-			$socialSecurityNumber = $_POST['socialSecurityNumber'];
-			$dateOfBirth = $_POST['dateOfBirth'];
-			$ethnicity = $_POST['ethnicity'];
-			$gender = $_POST['gender'];
-			$citizenship = $_POST['citizenship'];
 			$stmt->execute();
 
-			echo "New records created successfully";
+			echo "New accounted created";
 		}
 		catch(Exception $e){
 			echo "Error: " . $e->getMessage();
 		}
 		$conn = null;
-	}else{
-		echo "<table border=.5>";
-		foreach( $_POST as $stuff => $val ) {
-			echo "<tr>";
-			if( is_array( $stuff ) ) {
-				foreach( $stuff as $thing) {
-					echo $thing;
-				}
-			} else {
-				echo "<td>";
-				echo $stuff;
-				echo "</td>";
-				echo "<td>";
-				if($stuff=="socialSecurityNumber")
-					echo password_hash($val,PASSWORD_DEFAULT);
-				else
-					echo $val;
-				echo "</td>";
-			}
-			echo "</tr>";
-		}
-		echo "</table>";	
 	}
 ?>
