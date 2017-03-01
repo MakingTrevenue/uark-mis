050b00d8de0cc87e524556763821d1d2aa57e80b
Trevor Darby <tdarby51@gmail.com>
2017-03-01 14:41:33 -0600

    form validation

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,48 @@ function duplicate() {
 }
 
 //Checkbox Show On Clicked
-$(document).ready(function(){
+$(document).ready(function() {
     $('input[type="checkbox"]').click(function(){
         var inputValue = $(this).attr("value");
         $("." + inputValue).toggle();
     });
 })
+
+// Application Form Validation
+$(document).ready(function() {
+	$("#applicationform").validate({
+		rules: {
+			firstName: {
+				required: true,
+				minlength: 2
+			},
+
+			lastName: {
+				required: true,
+				minlength: 2
+			},
+
+			primaryEmail: {
+				required: true,
+				email: true
+			},
+
+		messages: {
+			firstName: {
+				required: "Please enter your first name.",
+				minlength: "Name must be longer than two characters."
+			},
+
+			lastName: {
+				required: "Please enter your last name.",
+				minlength: "Last name must be longer than two characters."
+			},
+
+			primaryEmail: {
+				required: "Please enter your email address.",
+				email: "Email must be in format of email@domain.com."
+			}
+		}
+
+	});
+});
