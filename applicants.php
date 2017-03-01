2962629d3d402db290f342ec16247db0ab8743ce
nwsm <eknewsom@uark.edu>
2017-03-01 10:00:59 -0600

    Fixed showing id instead of name

--- applicants.php
+++ applicants.php
@@ -688,7 +688,7 @@
     <?php 
     while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
         echo "<tr>
-                <td> <a href='http://uark.us/applicants.php?appID=" . $row[0] . "'>" . $row[34] . " " . $row[36] . "</a></td>
+                <td> <a href='http://uark.us/applicants.php?appID=" . $row[0] . "'>" . $row[35] . " " . $row[37] . "</a></td>
                 <td>" . $row[2] . " " . $row[3] . "</td>
                 <td>" . $row[4] . "</td>
                 <td>" . $row[11] . "</td>
