c5b20cca1f4b686b10bd57021a9896080379c77a
nwsm <eknewsom@uark.edu>
2017-03-01 15:09:46 -0600

    Get filename from POST

--- document.php
+++ document.php
@@ -2,7 +2,7 @@
 
 
 
-    $file="../docs/test";
+    $file="../docs/" . $_POST['filename'];
     header('Content-type: application/pdf');
     header('Content-disposition: inline; filename="' . $file . '"');
     header('Content-Transfer-Encoding: binary');
