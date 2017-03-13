3442c8145568f1d00997967bd59e380429f94d97
Trevor Darby <tdarby51@gmail.com>
2017-03-13 16:35:47 -0500

    validation looks nice now

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,25 @@ $(document).ready(function() {
     });
 })
 
+//jQuery Validation Override
+$.validator.setDefaults({
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
 //Form Validation
 $("#applicationform").validate({
     rules: {
