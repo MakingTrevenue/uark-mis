85a511152ea693838b7cf2303178b999dd2ae408
nwsm <eknewsom@uark.edu>
2017-03-14 03:30:03 -0500

    Maybe I should work on stuff I'm supposed to be working on

--- js/scripts.js
+++ js/scripts.js
@@ -48,9 +48,6 @@ $("#applicationform").validate({
     focusInvalid: false,
     invalidHandler: function(form, validator) {
 
-        if (!validator.numberOfInvalids())
-            return;
-
         $('html, body').animate({
             scrollTop: $(validator.errorList[0].element).offset().top
         }, 2000);
