ef12c62d067ede5e3a560e4e6b0e75fe0cf57e35
Trevor Darby <tdarby51@gmail.com>
2017-03-14 12:57:39 -0500

    git wtf

--- database/class_instructor_insert_statements.sql
+++ database/class_instructor_insert_statements.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 INSERT INTO `class_instructor` VALUES (1, 3, 'Fall', '2017');
 INSERT INTO `class_instructor` VALUES (2, 3, 'Fall', '2017');
 INSERT INTO `class_instructor` VALUES (3, 3, 'Fall', '2017');
@@ -18,4 +19,26 @@ INSERT INTO `class_instructor` VALUES (17, 18, 'Fall', '2017');
 INSERT INTO `class_instructor` VALUES (18, 6, 'Fall', '2017');
 INSERT INTO `class_instructor` VALUES (19, 6, 'Fall', '2017');
 INSERT INTO `class_instructor` VALUES (20, 1, 'Fall', '2017');
+=======
+INSERT INTO `class_instructor` VALUES (1, 3, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (2, 3, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (3, 3, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (4, 2, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (5, 2, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (6, 4, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (7, 4, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (8, 7, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (9, 7, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (10, 11, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (11, 11, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (12, 13, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (13, 13, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (14, 15, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (15, 15, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (16, 18, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (17, 18, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (18, 6, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (19, 6, 'Fall', '2017');
+INSERT INTO `class_instructor` VALUES (20, 1, 'Fall', '2017');
+>>>>>>> Stashed changes
 INSERT INTO `class_instructor` VALUES (21, 1, 'Fall', '2017');
