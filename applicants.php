e320c476c0fa1bcd52e5eb2a024d0bde0ef69974
nwsm <eknewsom@uark.edu>
2017-03-14 16:30:10 -0500

    Just trying

--- applicants.php
+++ applicants.php
@@ -480,7 +480,10 @@
                 </div>
                 <div class="panel-body">
                     <h4>
+                        $check = $stmt->fetch(PDO::FETCH_BOTH);
+                        <?php while($check = $stmt->fetch(PDO::FETCH_BOTH)){ ?>
                         <a href="/document.php?id=<?php echo $check['filename']; ?>" target="_blank" rel="noopener noreferrer">Resume</a><br>
+                        <?php } ?>
                     </h4>                 
                 </div>
             </div>
