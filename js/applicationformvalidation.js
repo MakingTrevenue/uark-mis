e766b7c6a460b09ddcd34f14118cc0518e505f80
Trevor Darby <tdarby51@gmail.com>
2017-03-14 18:01:27 -0500

    more form validation stuff

--- js/applicationformvalidation.js
+++ js/applicationformvalidation.js
@@ -1,36 +1,3 @@
-// Scripts
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
-});
-
-//Duplicate Function
-document.getElementById('duplicateButton').onclick = duplicate;
-
-var i = 0;
-var original = document.getElementById('collegeDuplicate');
-
-function duplicate() {
-    var clone = original.cloneNode(true); // "deep" clone
-    clone.id = "collegeDuplicate" + ++i; // there can only be one element with an ID
-    original.parentNode.appendChild(clone);
-    return false; //prevent form submission
-}
-
-//Checkbox Show On Clicked
-$(document).ready(function() {
-    $('input[type="checkbox"]').click(function(){
-        var inputValue = $(this).attr("value");
-        $("." + inputValue).toggle();
-    });
-})
-
-//jQuery Masked Input
-$(document).ready(function() {
-  $("#primaryPhone").inputmask("(999) 999-9999");
-  $("#secondaryPhone").inputmask("(999) 999-9999");
-});
-
-
 //jQuery Validation Override
 $.validator.setDefaults({
     highlight: function(element) {
