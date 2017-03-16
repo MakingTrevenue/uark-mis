2597004b987c9d2545c880510b85a492fc9cb78b
Trevor Darby <tdarby51@gmail.com>
2017-03-16 12:59:40 -0500

    updated footer

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 	<!-- Custom Scripts -->    
     <script type="text/javascript" src="./js/applicationformvalidation.js"></script>
     <script type="text/javascript" src="./js/scripts.js"></script>
-    
+
 </body>
 
 <footer>
-	<p>
+	<p class="text-center text-muted">
 	DISCLAIMER: This is a student project. This is NOT the official website for the University of Arkansas and is in no way affiliated with the University of Arkansas. Do NOT enter any personal information. To apply to the University of Arkansas, please visit their official website <a href="https://application.uark.edu/">here</a>.
 	</p>
 </footer>
