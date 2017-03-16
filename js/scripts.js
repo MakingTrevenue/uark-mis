11beb8222b5cbd785e89b8b0055649c9e89a4a05
Trevor Darby <tdarby51@gmail.com>
2017-03-16 13:26:31 -0500

    pls work

--- js/scripts.js
+++ js/scripts.js
@@ -24,11 +24,14 @@ function filterTable() {
 }
 
 // Navbar Active Tabs
-window.onload=function () {
-    $('#applicationTab').addClass('active');
-    $('#applicantsTab').addClass('active');
-    $('#advisingTab').addClass('active');
-};
+var url = window.location;
+// Will only work if string in href matches with location
+$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+// Will also work for relative and absolute hrefs
+$('ul.nav a').filter(function() {
+    return this.href == url;
+}).parent().addClass('active');
 
 //Duplicate Function
 document.getElementById('duplicateButton').onclick = duplicate;
