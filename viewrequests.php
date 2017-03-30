73b1c6e87e2a036cbd67d0667f3d5905c582958e
nwsm <eknewsom@uark.edu>
2017-03-30 17:42:40 -0500

    viewrequests pulls requests from db

--- viewrequests.php
+++ viewrequests.php
@@ -1,11 +1,23 @@
+<?php
+include 'php/functions.php';
+try{
+    $conn=createPDO();
+    $stmt = $conn->prepare("SELECT * FROM ga_request JOIN instructor ON ga_request.facultyID=instructor.instructorID");
+    $stmt->execute();    
+}catch(Exception $e){
+   echo $e; 
+}
+
+?>
+
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>jQuery UI Droppable - Default functionality</title>
   <style>
-  #requests { width: 200px; height: 350px; float: left; border-style: groove; }
-  #students { width: 200px; height: 350px; float: left; border-style: groove;} 
+  #requests { width: 200px; display:inline-block; float: left; border-style: groove; }
+  #students { width: 200px; display:inline-block; float: left; border-style: groove;} 
   .wrapper { float: left;}
   .formclass { float: left; border-style: groove;} 
   .request { margin: auto; width: 175px; height: 100px; border-style: groove; background-color: white;}
@@ -16,9 +28,14 @@
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script>
   pairs={};
-  pairs['r1']='i1';
-  pairs['r2']='i2';
-  pairs['r3']='i3';
+    <?php
+    $i=0;
+    while ($row = $stmt->fetch(PDO::FETCH_OBJ, PDO::FETCH_ORI_NEXT)) {
+        echo "pairs['r".$i."']='i".$i."';";
+        $i++;
+    }
+    ?>
+
   function updateInput(inputid,studentid){
 	document.getElementById(pairs[inputid]).value=document.getElementById(studentid).innerHTML;
   }
@@ -56,32 +73,45 @@
 <body>
 <div class="wrapper">
 	<div id="requests">
-	  <div id="r1" class="request">
-		request 1
-	  </div>
-	  <div id="r2" class="request">
-		request 2
-	  </div>
-	  <div id="r3" class="request">
-		request 3
-	  </div>  
+        <?php
+        $stmt->execute(); 
+        $i=0;
+        while ($row = $stmt->fetch(PDO::FETCH_OBJ, PDO::FETCH_ORI_NEXT)) {
+            echo "
+            <div id='r".$i."' class='request'>
+		        ".$row->firstName . " " . $row->lastName
+                 ."<br>". $row->dept
+                 ."<br>Hours: ".$row->hours."
+	        </div> ";
+            $i++;
+        }
+        ?>
+ 
 	</div>
 	 
 	<div id="students">
-	  <div id="s1" class="student">student 1</div>
-	  <div id="s2" class="student">student 2</div>
-	  <div id="s3" class="student">student 3</div> 
+	  <div id="s0" class="student">student 1</div>
+	  <div id="s1" class="student">student 2</div>
+	  <div id="s2" class="student">student 3</div> 
 	</div>
 	<br>
-	GA Request Form (to be hidden):
-	<form id="garequestform">
-		Request 1: <input id="i1" value=""><br>
-		Request 2: <input id="i2" value=""><br>
-		Request 3: <input id="i3" value=""><br>
-		<input type="submit" value="Submit (Won't be hidden)">
-	</form>
 	
-<div>
+    <div class='wrapper'>
+        GA Request Form (to be hidden):
+        <form id="garequestform">
+            <?php
+            $stmt->execute(); 
+            $i=0;
+            while ($row = $stmt->fetch(PDO::FETCH_OBJ, PDO::FETCH_ORI_NEXT)) {
+                echo
+                    "Request ".$row->requestID.": <input id='i".$i."' value=''><br>";
+                $i++;
+            }
+            ?>        
+            <input type="submit" value="Submit (Won't be hidden)">
+        </form>
+    </div>
+</div>
 
  
 </body>
