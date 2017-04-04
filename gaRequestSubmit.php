90ee4c0866d49af6937ace17819cc167c021d18e
Trevor Darby <tdarby51@gmail.com>
2017-04-04 13:09:49 -0500

    need local testing

--- gaRequestSubmit.php
+++ gaRequestSubmit.php
@@ -7,7 +7,7 @@ try {
 
 	$conn=createPDO();
 
-	$stmt = $conn->prepare("INSERT INTO request (reason, hours, description, internet, wordProcessing, spreadsheets, programming, database, sap, statisticalPackages,  programmingLanguages,  writing,  editing,  english, grading, otherSkills, multipleGAs, multipleGAsNumber, gaComments) VALUES  (:reason, :hours, :description, :internet, :wordProcessing, :spreadsheets, :programming, :database, :sap, :statisticalPackages, :programmingLanguages, :writing, :editing, :english, :grading, :otherSkills, :multipleGAs, :multipleGAsNumber, :gaComments)");
+	$stmt = $conn->prepare("INSERT INTO request (reason, hours, description, internet, wordProcessing, spreadsheets, programming, `database`, sap, statisticalPackages,  programmingLanguages,  writing,  editing,  english, grading, otherSkills, multipleGAs, multipleGAsNumber, gaComments) VALUES  (:reason, :hours, :description, :internet, :wordProcessing, :spreadsheets, :programming, :database, :sap, :statisticalPackages, :programmingLanguages, :writing, :editing, :english, :grading, :otherSkills, :multipleGAs, :multipleGAsNumber, :gaComments)");
 
 	$stmt->bindParam(':reason', $_POST['reason']);
 	$stmt->bindParam(':hours', $_POST['hours']);
