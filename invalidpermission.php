15899049254110d2959bc5cd60c0dac0dc0280f1
Trevor Darby <tdarby51@gmail.com>
2017-04-04 19:25:18 -0500

    pretty formatting for invalid permissions

--- invalidpermission.php
+++ invalidpermission.php
@@ -1,4 +1,9 @@
 <?php
-    include 'header.php';
-    echo "You need " . $_GET['e'] . " permissions to access this page."
+    include 'header.php'; ?>
+    <h1 class="text-center text-danger">
+    	You need 
+    	<b> <?php echo $_GET['e']; ?> </b>
+    	permissions to access this page.
+    </h1>
+    <?php
 ?>
