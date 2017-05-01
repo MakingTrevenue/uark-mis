7ad83d9a89fffdff6b97e5dfc2c6c9f755d1a2c6
nwsm <eknewsom@uark.edu>
2017-05-01 00:14:37 -0500

    Another attempt to fix for iOS

--- viewEvaluations.php
+++ viewEvaluations.php
@@ -27,7 +27,7 @@
     $i=0;
     while ($row = $stmt->fetch(PDO::FETCH_OBJ, PDO::FETCH_ORI_NEXT)) {
         $i++;
-        echo "<tr data-toggle='modal' data-target='#evalModal".$i."'>
+        echo "<tr href='#' data-toggle='modal' data-target='#evalModal".$i."'>
                 <td>" . $row->studentID . "</td>
                 <td>" . $row->firstName . " " . $row->lastName . "</td>
                 <td>" . $row->overall . "</td>                
