4c86c140104f1ec9c14f0f43206717feaa987394
nwsm <eknewsom@uark.edu>
2017-05-01 13:45:36 -0500

    Changed some execution time limits, added script to create users from students

---
+++ createUsersFromStudents.php
@@ -0,0 +1,36 @@
+<?php
+    include "php/functions.php";
+    ini_set('max_execution_time', 180);
+
+    $conn=createPDO();
+
+    $select = $conn->prepare("SELECT * FROM student WHERE userID IS NULL");
+    $select->execute();
+
+    $insert = $conn->prepare("INSERT INTO user (name,  email,  username,  password, gaRole) 
+									   VALUES (:name, :email, :username, :password, 1)");
+
+    $update = $conn->prepare("UPDATE student SET userID=:userID WHERE studentID=:studentID");
+
+    while ($row = $select->fetch(PDO::FETCH_OBJ, PDO::FETCH_ORI_NEXT)) {
+        if(empty($row->preferredName) || $row->preferredName==NULL)
+            $name=$row->firstName . " " . $row->lastName;
+        else
+            $name=$row->preferredName;
+
+            
+
+        $insert->bindValue(':name', $name);
+        $insert->bindValue(':email', $row->primaryEmail);
+        $insert->bindValue(':username', $row->firstName . $row->lastName);
+        $insert->bindValue(':password', password_hash("1234",PASSWORD_DEFAULT));
+        $insert->execute();
+
+        $userid=$conn->lastInsertId();
+
+        $update->bindValue(':userID',$userid);
+        $update->bindValue(':studentID',$row->studentID);
+        $update->execute();
+    }
+
+?>
--- php/functions.php
+++ php/functions.php
@@ -6,7 +6,8 @@
             $username = $config["username"];
             $password = $config["password"];
             $dbname = $config["dbname"];
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, 
+                array(PDO::ATTR_TIMEOUT => 180,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);            
             return($conn);
         }
